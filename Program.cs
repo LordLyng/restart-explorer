b20d87c2846eddf05ce69cf0fc03619cadaf7338
Morten Hau Lyng <mhl@it-minds.dk>
2024-02-16 10:57:03 +0100

    Init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+var processes = Process.GetProcessesByName("explorer");
+while (processes.Length > 0)
+{
+    var process = processes.First();
+    process.Kill(true);
+    process.WaitForExit();
+
+    processes = Process.GetProcessesByName("explorer");
+}
+
+Process.Start(Path.Combine(Environment.GetEnvironmentVariable("windir")!, "explorer.exe"));
